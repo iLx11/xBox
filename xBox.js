228af245f51e6e6ca3d33fe9d51b0c06cc5c02e1
l <l>
2024-11-18 10:11:55 +0800

    OPTIMISE_CONFIG

--- xBox.js
+++ xBox.js
@@ -4,8 +4,14 @@ class XBox {
   static popContent = null
   static promptContent = null
 
-  // 提示文本
-  static promptMes(mes = '', {type = 'info', dur = 2000} = {}) {
+  /********************************************************************************
+   * @brief: 提示文本
+   * @param {*} mes 消息文本
+   * @param {*} config 消息配置
+   * @return {*}
+   ********************************************************************************/
+  static promptMes(mes = '', config) {
+    let boxConfig = Object.assign({}, defaultBoxConfig, config)
     // 创建容器
     const promptContentID = 'prompt-content'
     this.promptContent = document.querySelector('#' + promptContentID)
@@ -18,16 +24,22 @@ class XBox {
     const promptBox = document.createElement('div')
     promptBox.className = 'prompt-box'
     promptBox.innerHTML = `${defaultIcon[0]}<div class="message-box">${mes}</div>`
-    
-    this.animatationHandle(promptBox,  this.promptContent, () => {
+
+    this.animatationHandle(promptBox, this.promptContent, () => {
       promptBox.style.display = 'none'
       promptBox.remove()
       this.promptContent.remove()
     })
   }
 
-  // 弹出消息
-  static popMes(mes = '', { type = 'info', dur = 2000 } = {}) {
+  /********************************************************************************
+   * @brief: 弹出消息
+   * @param {*} mes 消息文本
+   * @param {*} config 消息配置
+   * @return {*}
+   ********************************************************************************/
+  static popMes(mes = '', config) {
+    let boxConfig = Object.assign({}, defaultBoxConfig, config)
     // 创建容器
     const popContentID = 'pop-content'
     this.popContent = document.querySelector('#' + popContentID)
@@ -44,11 +56,26 @@ class XBox {
     this.animatationHandle(popBox, this.popContent, () => {
       popBox.style.display = 'none'
       popBox.remove()
+      if (boxConfig.callback) {
+        boxConfig.callback()
+      }
     })
   }
 
-  // 动画处理
-  static animatationHandle = (boxContent, boxParent, endCallback = null, beginCallback = null) => {
+  /********************************************************************************
+   * @brief: 动画处理
+   * @param {*} boxContent 动画 dom 元素
+   * @param {*} boxParent 动画主体的父元素
+   * @param {*} endCallback 移出动画完成回调
+   * @param {*} beginCallback 进入动画完成回调
+   * @return {*}
+   ********************************************************************************/
+  static animatationHandle = (
+    boxContent,
+    boxParent,
+    endCallback = null,
+    beginCallback = null
+  ) => {
     // 添加开始动画
     const beginAE = new KeyframeEffect(boxContent, defaultAE[0].in, {
       duration: 300,
@@ -69,7 +96,10 @@ class XBox {
           easing: 'ease-in',
         }
       )
-      const backAnimation = new Animation(backAnimationEffect, document.timeline)
+      const backAnimation = new Animation(
+        backAnimationEffect,
+        document.timeline
+      )
       backAnimation.play()
       backAnimation.onfinish = endCallback
     }, 2000)
